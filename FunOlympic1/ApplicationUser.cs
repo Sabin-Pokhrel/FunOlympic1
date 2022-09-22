7a2b9e5c73091eb7f3363dd9c3e8f555d605f26c
Sabin Pokhrel <sabinpokhrel525@gmail.com>
2022-09-22 18:02:04 +0545

    Add project files.

---
+++ FunOlympic1/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FunOlympic1
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? Name { get; set; }
+        public string? Country { get; set; }
+
+
+
+    }
+}
