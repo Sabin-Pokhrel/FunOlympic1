e5827208b4ff77abe99d485f15f7ed0cf99a79fb
Sabin Pokhrel <sabinpokhrel525@gmail.com>
2022-09-23 14:34:20 +0545

    edit user done but not working properly

---
+++ FunOlympic1/Pages/User/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FunOlympic1;
+using FunOlympic1.Data;
+
+namespace FunOlympic1.Pages.User
+{
+    public class EditModel : PageModel
+    {
+        private readonly FunOlympic1.Data.ApplicationDbContext _context;
+
+        public EditModel(FunOlympic1.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public ApplicationUser Video { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(String? id)
+        {
+            if (id == null || _context.ApplicationUsers == null)
+            {
+                return NotFound();
+            }
+
+            var video =  await _context.ApplicationUsers.FirstOrDefaultAsync(m => m.Id== id);
+            if (video == null)
+            {
+                return NotFound();
+            }
+            Video = video;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Video).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(Video.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool UserExists(string id)
+        {
+          return _context.ApplicationUsers.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ FunOlympic1/Pages/User/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FunOlympic1;
+using FunOlympic1.Data;
+
+namespace FunOlympic1.Pages.User
+{
+    public class IndexModel : PageModel
+    {
+        private readonly FunOlympic1.Data.ApplicationDbContext _context;
+
+        public IndexModel(FunOlympic1.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<ApplicationUser> UserList { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.ApplicationUsers != null)
+            {
+                UserList = await _context.ApplicationUsers.ToListAsync();
+            }
+        }
+    }
+}
