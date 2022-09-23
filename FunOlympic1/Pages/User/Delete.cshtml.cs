30517156e1fcdf07b1cbacb1ea4d33b35fcc1dcb
Sabin Pokhrel <sabinpokhrel525@gmail.com>
2022-09-23 15:29:28 +0545

    delete user done

--- FunOlympic1/Pages/User/Delete.cshtml.cs
+++ FunOlympic1/Pages/User/Delete.cshtml.cs
@@ -8,7 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using FunOlympic1;
 using FunOlympic1.Data;
 
-namespace FunOlympic1.Pages.Videos
+namespace FunOlympic1.Pages.User
+
 {
     public class DeleteModel : PageModel
     {
@@ -20,16 +21,16 @@ namespace FunOlympic1.Pages.Videos
         }
 
         [BindProperty]
-      public Video Video { get; set; }
+      public ApplicationUser Video { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+        public async Task<IActionResult> OnGetAsync(String? id)
         {
-            if (id == null || _context.Videos == null)
+            if (id == null || _context.ApplicationUsers == null)
             {
                 return NotFound();
             }
 
-            var video = await _context.Videos.FirstOrDefaultAsync(m => m.VideoID == id);
+            var video = await _context.ApplicationUsers.FirstOrDefaultAsync(m => m.Id == id);
 
             if (video == null)
             {
@@ -42,18 +43,18 @@ namespace FunOlympic1.Pages.Videos
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync(int? id)
+        public async Task<IActionResult> OnPostAsync(string? id)
         {
-            if (id == null || _context.Videos == null)
+            if (id == null || _context.ApplicationUsers == null)
             {
                 return NotFound();
             }
-            var video = await _context.Videos.FindAsync(id);
+            var video = await _context.ApplicationUsers.FindAsync(id);
 
             if (video != null)
             {
                 Video = video;
-                _context.Videos.Remove(Video);
+                _context.Users.Remove(Video);
                 await _context.SaveChangesAsync();
             }
 
